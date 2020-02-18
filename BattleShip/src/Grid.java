610e4c75e662cbf03f369899588fab0a77d3b3e8
JimmyLi2020 <lix@avonoldfarms.com>
2020-02-18 16:28:09 -0500

    Start work on grid and location class.

--- BattleShip/src/Grid.java
+++ BattleShip/src/Grid.java
@@ -1,3 +1,55 @@
 public class Grid {
-    
+
+    //attributes
+    Location[][] grid;
+
+    //constructor
+    public Grid(boolean[][] shipGrid) {
+        grid = new Location[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                grid[i][j] = new Location(shipGrid[i][j]);
+            }
+        }
+    }
+
+    boolean guess(int x, int y) {
+        return grid[x][y].guess();
+    }
+
+    int getStatus(int x, int y) {
+        return grid[x][y].getStatus();
+    }
+
+    class Location {
+
+        //attributes
+        boolean hasShip;
+        boolean hasBeenGuessed = false;
+
+        //constructor
+        Location(boolean hasShip) {
+            this.hasShip = hasShip;
+        }
+
+        // -1 = miss ; 0 = notGuessed ; 1 = hit
+        int getStatus() {
+            if (!hasBeenGuessed) {
+                return 0;
+            } else {
+                if (hasShip) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        }
+
+        //make guess
+        boolean guess() {
+            hasBeenGuessed = true;
+            return hasShip;
+        }
+    }
+
 }
